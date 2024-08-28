121b3d7490cd86ffdb5334733054fba68bf00841
Nnamani John <nnamanijohn123@gmail.com>
2024-08-28 21:51:08 +0100

    feat: accept input and print to screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("TODO CLI");
+    loop {
+        print!("Todo > ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => println!("{}", input.trim()),
+            Err(e) => println!("An error occurred: {e}"),
+        }
+    }
 }
